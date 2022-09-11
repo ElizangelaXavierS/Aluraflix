fde4ab430bbb57ed3c879d2d4aee1d59e1ac6ad9
Elizangela Xavier <elizangelaxavier913@gmail.com>
2022-09-11 15:48:06 -0300

    Adicionado HTML, CSS e JavaSript.

---
+++ script.js
@@ -0,0 +1,8 @@
+var listaDeFilmes =
+[ " https://a-static.mlcdn.com.br/1500x1500/filme-mulher-maravilha-dvd-warner-bros/playsetgames/6187172157/269e61e65f1b9ff9036ca489bc105336.jpg ", 
+" https://br.web.img2.acsta.net/medias/nmedia/18/92/08/07/20488996.jpg ", 
+" https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg "];
+
+for (var i = 0; i < listaDeFilmes.length; i++){
+    document.write(" <img src=" + listaDeFilmes[i] + ">");
+}
